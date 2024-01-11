de13a3bce3e5cc456f5d28e9a8c587dea76ead7b
KaraGalloway <kpuffin1393@hotmail.com>
2024-01-11 11:19:39 -0700

    adding notes file

---
+++ R_Commands_and_Notes.R
@@ -0,0 +1,11 @@
+#### COMMANDS ####
+
+# get working directory
+getwd()
+# get files in directory
+list.files()
+# get files in specific folder
+list.files(path = "Assignments/",recursive = TRUE)
+
+
+#### NOTES ####
