f4c0aff6843e2ebff6d2f13d41a5a59f72faf381
KaraGalloway <kpuffin1393@hotmail.com>
2024-02-06 19:04:33 -0700

    update to assignment 4

--- Assignments/Assignment_4/Assignment4.R
+++ Assignments/Assignment_4/Assignment4.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 library(tidyverse)
-Toxic_Release_Data <- read.csv("C:/Users/kpuff/DataClass/Data_Course_GALLOWAY/Utah_Toxic_Release_Spills_Inventory_2015-2018_EPA_20240206.csv")
+Toxic_Release_Data <- read.csv("./Utah_Toxic_Release_Spills_Inventory_2015-2018_EPA_20240206.csv")
 utah_county_data <- Toxic_Release_Data[Toxic_Release_Data$X7..COUNTY == "UTAH",]
 
 ggplot(utah_county_data, aes(x = X33..CHEMICAL)) +
